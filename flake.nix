8721eb8e7b12046e214598774f74b0812013d884
AyushmaanAggarwal <ayu02468@gmail.com>
2024-10-02 12:02:06 -0700

    Migrate from a stable to unstable nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,30 +2,22 @@
   description = "NixOS Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     #nixpkgs.follows = "nixos-cosmic/nixpkgs-stable";
     #nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
   };
 
   # outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, nixos-cosmic}@inputs: 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager}@inputs: 
+  outputs = { self, nixpkgs, home-manager}@inputs: 
     let
       system = "x86_64-linux";
       # system.nixos.label = (builtins.concatStringsSep "-" (builtins.sort (x: y: x < y) config.system.nixos.tags)) + config.system.nixos.version + "-SHA:${self.rev}";
-      overlay-unstable = final: prev: {
-        unstable = import nixpkgs-unstable {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      };
     in { 
       nixosConfigurations.ayu = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
-          ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
