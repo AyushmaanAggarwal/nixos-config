c0af11dc41260e3798f58cffba418ba95194162b
AyushmaanAggarwal <ayu02468@gmail.com>
2024-10-16 01:10:51 -0700

    Move binary caches to flake.nix

--- flake.nix
+++ flake.nix
@@ -18,6 +18,17 @@
         inherit system;
         specialArgs = { inherit inputs; }; # this is the important part
         modules = [
+          {
+            nix.settings = {
+              substituters = [
+                "https://hyprland.cachix.org"
+                "https://cache.nixos.org/"
+              ];
+              trusted-public-keys = [
+                "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+              ];
+            };
+          }
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
