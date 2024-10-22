ef5f1f35a436ee9a09c819b255ce578c57ea5812
AyushmaanAggarwal <ayu02468@gmail.com>
2024-10-22 13:18:05 -0700

    Add btrfs scrub to systemd services and change backup service timer

--- modules/system/systemd-configuration.nix
+++ modules/system/systemd-configuration.nix
@@ -12,8 +12,7 @@
       after = [ "graphical-session.target" ];
       serviceConfig = {
         Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"; Restart = "on-failure";
         RestartSec = 1;
         TimeoutStopSec = 10;
       };
@@ -35,6 +34,24 @@
         Type = "oneshot";
       };
     };
+
+    btrfs-scrub-notify = {
+      enable = false;
+      after = [ "network.target" ];
+      description = "Notify about btrfs scrub";
+      script = ''
+      #!/bin/sh
+      pushd /home/ayushmaan/.dotfiles/scripts/python/ > /dev/null
+      source venv/bin/activate
+      scrub_result=$(cat /home/ayushmaan/.local/custom-files/btrfs_scrub)
+      python3 slack_notification.py "$scrub_result"
+      popd > /dev/null
+      '';
+      serviceConfig = {
+        User = "ayushmaan";
+        Type = "oneshot";
+      };
+    };
   };
 
   systemd.services = {
@@ -48,6 +65,23 @@
         ExecStart = ''/etc/scripts/backup.sh'';
       };
     };
+
+    # Btrfs Scrub
+    btrfs-scrub = {
+      enable = false;
+      requires = [ "btrfs-scrub-notify" ];
+      before = [ "btrfs-scrub-notify" ];
+      after = [ "network.target" ];
+      description = "Btrfs Scrub";
+      script = ''
+      #!/bin/sh
+      /run/current-system/sw/bin/btrfs scrub start / > /home/ayushmaan/.local/custom-files/btrfs_scrub
+      '';
+      serviceConfig = {
+        User = "root";
+      };
+    };
+
   };
 
   # -------------------- 
@@ -58,8 +92,10 @@
     restic-backup = {
       wantedBy = [ "timers.target" ];
       timerConfig = {
-        OnBootSec = "20m";
-        OnUnitActiveSec = "24h";
+        OnBootSec = "10m";
+        #OnUnitActiveSec = "24h";
+        OnCalender = "daily";
+        Persistent = true;
         Unit = "restic-backup.service";
       };
     };
