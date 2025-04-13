c4587b36cbbf6184096fa9385f2f9b1cfb29bb6a
AyushmaanAggarwal <ayu02468@gmail.com>
2025-04-13 11:13:10 -0700

    Add second driver

--- modules/system/hardware/security.nix
+++ modules/system/hardware/security.nix
@@ -12,8 +12,8 @@
   # If simply enabling fprintd is not enough, try enabling fprintd.tod...
   services.fprintd.tod = {
     enable = true;
-    driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
-    # driver = pkgs.libfprint-2-tod1-elan; # Elan(04f3:0c4b) driver                    
+    # driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
+    driver = pkgs.libfprint-2-tod1-elan; # Elan(04f3:0c4b) driver                    
     # driver = pkgs.libfprint-2-tod1-vfs0090; # driver for 2016 ThinkPads 
     # driver = pkgs.libfprint-2-tod1-goodix-550a; # Goodix 550a driver (from Lenovo)  
   };
