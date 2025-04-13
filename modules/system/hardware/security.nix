a1cafaf89812867e96482cc6843e834d8918082e
AyushmaanAggarwal <ayu02468@gmail.com>
2025-04-13 11:15:29 -0700

    try 3rd driver

--- modules/system/hardware/security.nix
+++ modules/system/hardware/security.nix
@@ -13,8 +13,8 @@
   services.fprintd.tod = {
     enable = true;
     # driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
-    driver = pkgs.libfprint-2-tod1-elan; # Elan(04f3:0c4b) driver                    
-    # driver = pkgs.libfprint-2-tod1-vfs0090; # driver for 2016 ThinkPads 
+    # driver = pkgs.libfprint-2-tod1-elan; # Elan(04f3:0c4b) driver                    
+    driver = pkgs.libfprint-2-tod1-vfs0090; # driver for 2016 ThinkPads 
     # driver = pkgs.libfprint-2-tod1-goodix-550a; # Goodix 550a driver (from Lenovo)  
   };
 }
