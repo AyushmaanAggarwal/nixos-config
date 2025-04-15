20a61ee8bd65b651d040255d836a48dfde9a265c
AyushmaanAggarwal <ayu02468@gmail.com>
2025-04-14 21:18:10 -0700

    Remove fprintd as it doesn't work with the lg gram

--- modules/system/hardware/security.nix
+++ modules/system/hardware/security.nix
@@ -3,19 +3,5 @@
   # Normal Security
   security.polkit.enable = true;
 
-  # Fingerprint service
-  systemd.services.fprintd = {
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig.Type = "simple";
-  };
-  services.fprintd.enable = true;
-  # If simply enabling fprintd is not enough, try enabling fprintd.tod...
-  services.fprintd.tod = {
-    enable = true;
-    # driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
-    # driver = pkgs.libfprint-2-tod1-elan; # Elan(04f3:0c4b) driver                    
-    driver = pkgs.libfprint-2-tod1-vfs0090; # driver for 2016 ThinkPads 
-    # driver = pkgs.libfprint-2-tod1-goodix-550a; # Goodix 550a driver (from Lenovo)  
-  };
 }
 
