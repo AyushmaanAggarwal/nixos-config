b51cef3e6c08651ebbc61a25970922c800c2e482
AyushmaanAggarwal <ayu02468@gmail.com>
2025-04-15 12:58:44 -0700

    Add sops-nix

--- flake.nix
+++ flake.nix
@@ -9,12 +9,15 @@
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
     # nixpkgs.follows = "nixos-cosmic/nixpkgs";
     # nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
   };
 
   #outputs = { self, nixpkgs, nixpkgs-stable, nixpkgs-master, home-manager, nixos-cosmic}@inputs: 
-  outputs = { self, nixpkgs, nixpkgs-stable, home-manager }@inputs: 
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, sops-nix }@inputs: 
     let
       system = "x86_64-linux";
       overlay-pkgs = final: prev: {
@@ -26,21 +29,17 @@
     in { 
       nixosConfigurations.thegram = nixpkgs.lib.nixosSystem {
         inherit system;
+        specialArgs = { inherit inputs; };
         modules = [
           ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-pkgs ]; })
-          # nixos-cosmic.nixosModules.default
+          ./modules/configuration.nix
+          ./home-manager/configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
           }          
-          ./home-manager/configuration.nix
-          ./modules/configuration.nix
-          {
-            _module.args = {
-              inherit inputs;
-            };
-          }
+          sops-nix.nixosModules.sops
         ];
       };
     };
