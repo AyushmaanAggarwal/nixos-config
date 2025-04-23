ccb6d72bc9441494d6c128ca41aa4cec7e66c40b
AyushmaanAggarwal <ayu02468@gmail.com>
2025-04-22 17:52:47 -0700

    Remove cosmic (still a little buggy)

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -7,8 +7,8 @@
       ./applications.nix
 
       # --- Desktop Environment ---
-      ./desktop/cosmic.nix
-      #./desktop/hyprland.nix
+      #./desktop/cosmic.nix
+      ./desktop/hyprland.nix
       #./desktop/kde.nix # UNTESTED
       #./desktop/gnome.nix # UNTESTED
 
