65b553ada4cfdfea2b6b1295aa9058bcf3e93121
AyushmaanAggarwal <ayu02468@gmail.com>
2025-04-29 15:24:12 -0700

    Add yubikey

--- modules/system/hardware/security.nix
+++ modules/system/hardware/security.nix
@@ -3,5 +3,20 @@
   # Normal Security
   security.polkit.enable = true;
 
+  # Add Yubikey Support
+  security.pam.services = {
+    login.u2fAuth = true;
+    sudo.u2fAuth = true;
+  };
+
+  services.udev.extraRules = ''
+      ACTION=="remove",\
+       ENV{ID_BUS}=="usb",\
+       ENV{ID_MODEL_ID}=="0402",\
+       ENV{ID_VENDOR_ID}=="1050",\
+       ENV{ID_VENDOR}=="Yubico",\
+       RUN+="hyprlock"
+  '';
+
 }
 
