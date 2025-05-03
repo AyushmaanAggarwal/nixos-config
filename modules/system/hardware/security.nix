2f325c6709e1ad8a680791564053f360cabfdf94
AyushmaanAggarwal <ayu02468@gmail.com>
2025-05-02 18:03:34 -0700

    Remove bind udev as remove udev works now

--- modules/system/hardware/security.nix
+++ modules/system/hardware/security.nix
@@ -10,7 +10,6 @@
   };
 
   services.udev.extraRules = ''
-    ACTION=="bind", ENV{ID_VENDOR}=="Yubico", ENV{ID_VENDOR_ID}=="1050", ENV{ID_MODEL_ID}=="0402", RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
     ACTION=="remove", ENV{ID_VENDOR_FROM_DATABASE}=="Yubico.com", ENV{ID_FIDO_TOKEN}=="1", ENV{ID_SECURITY_TOKEN}=="1", RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
   '';
 
