abfc0189eb5e6bc1a2d2d5ef99b8627d9caab732
AyushmaanAggarwal <ayu02468@gmail.com>
2025-05-25 21:37:44 -0700

    Add userChrome to firefox

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -55,6 +55,22 @@ in
           };
         };
       };
+      userChrome = ''
+      /* hides the title bar */
+      #titlebar {
+        visibility: collapse;
+      }
+      
+      /* hides the sidebar */
+      #sidebar-header {
+        visibility: collapse !important;
+      }
+      
+      /* hides the native tabs */
+      #TabsToolbar {
+        visibility: collapse;
+      }
+      '';
     };
 
     policies = {
