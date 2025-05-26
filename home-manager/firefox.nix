6b3bf5abbdf5c5e29ff05bd6188ed8b7377dee46
AyushmaanAggarwal <ayu02468@gmail.com>
2025-05-25 20:58:52 -0700

    Move policies outside of profiles in firefox hm

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -55,77 +55,77 @@ in
           };
         };
       };
+    };
 
-      policies = {
-        DisableTelemetry = true;
-        DisableFirefoxStudies = true;
-        EnableTrackingProtection = {
-          Value= true;
-          Locked = true;
-          Cryptomining = true;
-          Fingerprinting = true;
-        };
-        DisablePocket = true;
-        DisableFirefoxAccounts = false;
-        DisableAccounts = false;
-        OverrideFirstRunPage = "";
-        OverridePostUpdatePage = "";
-        DontCheckDefaultBrowser = true;
-        DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
-        DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
-        SearchBar = "unified"; # alternative: "separate"
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value= true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      DisablePocket = true;
+      DisableFirefoxAccounts = false;
+      DisableAccounts = false;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      DontCheckDefaultBrowser = true;
+      DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
+      DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
+      SearchBar = "unified"; # alternative: "separate"
 
-        /* ---- EXTENSIONS ---- */
-        # Check about:support for extension/add-on ID strings.
-        # Valid strings for installation_mode are "allowed", "blocked",
-        # "force_installed" and "normal_installed".
-        ExtensionSettings = builtins.listToAttrs [
-          (extension "ublock-origin" "uBlock0@raymondhill.net")
-          (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}") # Bitwarden
-          (extension "darkreader" "addon@darkreader.org") # Dark Reader
-          (extension "sidebery" "{3c078156-979c-498b-8990-85f7987dd929}") # Sideberry
-        ];
+      /* ---- EXTENSIONS ---- */
+      # Check about:support for extension/add-on ID strings.
+      # Valid strings for installation_mode are "allowed", "blocked",
+      # "force_installed" and "normal_installed".
+      ExtensionSettings = builtins.listToAttrs [
+        (extension "ublock-origin" "uBlock0@raymondhill.net")
+        (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}") # Bitwarden
+        (extension "darkreader" "addon@darkreader.org") # Dark Reader
+        (extension "sidebery" "{3c078156-979c-498b-8990-85f7987dd929}") # Sideberry
+      ];
  
-        /* ---- PREFERENCES ---- */
-        # Check about:config for options.
-        Preferences = { 
-          "browser.tabs.groups.enabled" = true;
-          "browser.toolbarbuttons.introduced.sidebar-button" = true;
-          "datareporting.usage.uploadEnabled" = true;
+      /* ---- PREFERENCES ---- */
+      # Check about:config for options.
+      Preferences = { 
+        "browser.tabs.groups.enabled" = true;
+        "browser.toolbarbuttons.introduced.sidebar-button" = true;
+        "datareporting.usage.uploadEnabled" = true;
 
 
-          "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
-          "extensions.pocket.enabled" = !lock;
-          "extensions.screenshots.disabled" = lock;
-          "browser.topsites.contile.enabled" = !lock;
-          "browser.formfill.enable" = false;
-          "browser.search.suggest.enabled" = false;
-          "browser.search.suggest.enabled.private" = false;
-          "browser.urlbar.suggest.searches" = false;
-          "browser.urlbar.showSearchSuggestionsFirst" = !lock;
-          "browser.newtabpage.activity-stream.feeds.section.topstories" = !lock;
-          "browser.newtabpage.activity-stream.feeds.snippets" = !lock;
-          "browser.newtabpage.activity-stream.section.highlights.includePocket" = !lock;
-          "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = !lock;
-          "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = !lock;
-          "browser.newtabpage.activity-stream.section.highlights.includeVisited" = !lock;
-          "browser.newtabpage.activity-stream.showSponsored" = !lock;
-          "browser.newtabpage.activity-stream.system.showSponsored" = !lock;
-          "browser.newtabpage.activity-stream.showSponsoredTopSites" = !lock;
+        "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+        "extensions.pocket.enabled" = !lock;
+        "extensions.screenshots.disabled" = lock;
+        "browser.topsites.contile.enabled" = !lock;
+        "browser.formfill.enable" = false;
+        "browser.search.suggest.enabled" = false;
+        "browser.search.suggest.enabled.private" = false;
+        "browser.urlbar.suggest.searches" = false;
+        "browser.urlbar.showSearchSuggestionsFirst" = !lock;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = !lock;
+        "browser.newtabpage.activity-stream.feeds.snippets" = !lock;
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" = !lock;
+        "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = !lock;
+        "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = !lock;
+        "browser.newtabpage.activity-stream.section.highlights.includeVisited" = !lock;
+        "browser.newtabpage.activity-stream.showSponsored" = !lock;
+        "browser.newtabpage.activity-stream.system.showSponsored" = !lock;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = !lock;
 
-          "browser.newtabpage.activity-stream.feeds.telemetry" = telemetry;
-          "browser.newtabpage.activity-stream.telemetry" = telemetry;
-          "browser.newtabpage.activity-stream.telemetry.ut.events" = telemetry;
-          "browser.search.serpEventTelemetryCategorization.enabled" = telemetry;
-          "browser.search.serpEventTelemetryCategorization.regionEnabled" = telemetry;
-          "dom.security.unexpected_system_load_telemetry_enabled" = telemetry;
-          "identity.fxaccounts.telemetry.clientAssociationPing.enabled" = telemetry;
-          "network.trr.confirmation_telemetry_enabled" = telemetry;
-          "nimbus.telemetry.targetingContextEnabled" = telemetry;
-          "telemetry.fog.artifact_build" = telemetry;
-          "telemetry.fog.init_on_shutdown" = telemetry;
-          "toolkit.telemetry.enabled" = telemetry;
-        };
+        "browser.newtabpage.activity-stream.feeds.telemetry" = telemetry;
+        "browser.newtabpage.activity-stream.telemetry" = telemetry;
+        "browser.newtabpage.activity-stream.telemetry.ut.events" = telemetry;
+        "browser.search.serpEventTelemetryCategorization.enabled" = telemetry;
+        "browser.search.serpEventTelemetryCategorization.regionEnabled" = telemetry;
+        "dom.security.unexpected_system_load_telemetry_enabled" = telemetry;
+        "identity.fxaccounts.telemetry.clientAssociationPing.enabled" = telemetry;
+        "network.trr.confirmation_telemetry_enabled" = telemetry;
+        "nimbus.telemetry.targetingContextEnabled" = telemetry;
+        "telemetry.fog.artifact_build" = telemetry;
+        "telemetry.fog.init_on_shutdown" = telemetry;
+        "toolkit.telemetry.enabled" = telemetry;
       };
     };     
   };
