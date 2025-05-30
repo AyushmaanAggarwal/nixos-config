36299105c073cd35f1425b6c153c4bcbb2113f5c
AyushmaanAggarwal <ayu02468@gmail.com>
2025-05-30 10:34:08 -0700

    Add Ollama support for future

---
+++ modules/server/services/ollama.nix
@@ -0,0 +1,39 @@
+# Never tested
+{ inputs, config, lib, pkgs, ... }:
+{
+  options = {
+    ollama.enable = lib.mkOption {
+      type = lib.types.bool;
+      description = "Enable Ollama AI";
+      default = false;
+    };
+  };
+  config = lib.mkIf (config.ollama.enable) {
+    # -------------------- 
+    # Nvidia Hardware Drivers
+    # -------------------- 
+    hardware.graphics.enable = true;
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia.open = false;
+
+    # -------------------- 
+    # Ollama Server
+    # -------------------- 
+    services.ollama = {
+      enable = true;
+      acceleration = "cuda";
+      # Optional: preload models, see https://ollama.com/library
+      loadModels = [ "llama3.2:3b" ];
+    };
+    services.open-webui.enable = true;
+
+    # -------------------- 
+    # Caddy SSL Cert
+    # -------------------- 
+    caddy = {
+      enable = true;
+      port = 8080;
+    };
+    
+  };
+}
