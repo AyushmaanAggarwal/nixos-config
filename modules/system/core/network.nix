297c5425f403fa5e70e04da5de0732bead3c0a06
AyushmaanAggarwal <ayu02468@gmail.com>
2025-05-30 22:39:52 -0700

    Add more settings to try reducing doh cpu usage

--- modules/system/core/network.nix
+++ modules/system/core/network.nix
@@ -39,6 +39,14 @@
       require_nolog = true;
       require_nofilter = false;
 
+      cache_size = 8192;
+      cache_min_ttl = 2400;
+      cache_max_ttl = 86400;
+      cache_neg_min_ttl = 60;
+      cache_neg_max_ttl = 600;
+
+      enable_hot_reload = false;
+
       bootstrap_resolvers = ["100.100.100.100:53" "9.9.9.9:53" "149.112.112.112:53" "1.1.1.1:53"];
       forwarding_rules = "/etc/nixos/services/networking/forwarding-rules.txt";
       sources.public-resolvers = {
@@ -46,6 +54,7 @@
           "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
           "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
         ];
+        refresh_delay = 72;
         cache_file = "/var/lib/dnscrypt-proxy/public-resolvers.md";
         minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
       };
@@ -54,10 +63,9 @@
         "quad9-doh-ip4-port443-filter-pri"
         "quad9-doh-ip4-port443-filter-alt"
         "quad9-doh-ip4-port443-filter-alt2"
-        # Removed dot as performance is close to two times slower over multiple days of thousands of requests
-        #"quad9-doh-ip4-port5053-filter-pri"
-        #"quad9-doh-ip4-port5053-filter-alt"
-        #"quad9-doh-ip4-port5053-filter-alt2"
+        "quad9-doh-ip4-port5053-filter-pri"
+        "quad9-doh-ip4-port5053-filter-alt"
+        "quad9-doh-ip4-port5053-filter-alt2"
       ];
     };
   };
