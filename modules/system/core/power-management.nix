a1c36f515753b177083074b488766b0ac7474641
AyushmaanAggarwal <ayu02468@gmail.com>
2025-06-02 14:13:28 -0700

    Try with tlp disabled

--- modules/system/core/power-management.nix
+++ modules/system/core/power-management.nix
@@ -3,7 +3,7 @@
   services.thermald.enable = true;
   powerManagement.enable = true;
   services.tlp = {
-    enable = true;
+    enable = false;
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
