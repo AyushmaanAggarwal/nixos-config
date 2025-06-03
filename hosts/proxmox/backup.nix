62d54ae78cffbbf7b63f6525f122fd9bda58c3d9
AyushmaanAggarwal <ayu02468@gmail.com>
2025-06-03 11:18:08 -0700

    Add another authorized key

--- hosts/proxmox/backup.nix
+++ hosts/proxmox/backup.nix
@@ -12,4 +12,10 @@
     ../../modules/server/default.nix
   ];
   networking.hostName = "backup";
+
+
+  users.users.proxmox.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEa53AGMV87VUquUKyQ2NlqmZiN7OVV438VLUe6hYJU2"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOmhA7UsDVaSC7A+CLcKnKkYuSjgObaauAFJWdjHmK1X ayushmaan@thegram"
+  ];
 }
