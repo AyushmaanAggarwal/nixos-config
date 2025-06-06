fbb612304b99739cf2b178303bb989aaa2af524a
AyushmaanAggarwal <ayu02468@gmail.com>
2025-06-06 15:20:14 -0700

    Revise kitty settings

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -5,11 +5,18 @@
     enableGitIntegration = true;
     shellIntegration.enableZshIntegration = true;
     settings = {
-      scrollback_lines = 10000;
+      # Fonts
       font_family = "FiraCode Nerd Font Mono Reg";
+      font_size = 13;
       symbol_map = "U+E5FF Jetbrains Mono";
-      window_padding_width = 10;
+      
+      # Environment
       editor = "nvim";
+      shell = "zsh";
+
+      # Visual
+      scrollback_lines = 10000;
+      window_padding_width = 10;
       linux_display_server = "Wayland";
     };  
   };
