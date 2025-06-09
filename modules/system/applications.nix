bf837f9e49d2b3a53ad9111d0f78a3a598c3a322
AyushmaanAggarwal <ayu02468@gmail.com>
2025-06-09 10:41:35 -0700

    Add additional packages steam for gamescope

--- modules/system/applications.nix
+++ modules/system/applications.nix
@@ -31,6 +31,20 @@
     steam = {
       enable = true;
       gamescopeSession.enable = true; # For gamescope
+      package = pkgs.steam.override {
+        extraPkgs = pkgs': with pkgs'; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
+      };
     };
     gamescope = { # Steam Virtualized Compositor
       enable = true;
