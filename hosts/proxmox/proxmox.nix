c2b7c42ad5b60047b143d887f49120abf24e42c8
AyushmaanAggarwal <ayu02468@gmail.com>
2025-06-13 19:23:31 -0700

    Switch mkIf with optionalAtters

--- hosts/proxmox/proxmox.nix
+++ hosts/proxmox/proxmox.nix
@@ -23,8 +23,8 @@ in
     ../../modules/server/default.nix
   ];
   config = lib.mkMerge [
-    (lib.mkIf hostname-option { ${hostname}.enable = true; })
-    (lib.mkIf sshWithoutYubikey {
+    (lib.optionalAttrs hostname-option { ${hostname}.enable = true; })
+    (lib.optionalAttrs sshWithoutYubikey {
       users.users.${username}.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEa53AGMV87VUquUKyQ2NlqmZiN7OVV438VLUe6hYJU2"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOmhA7UsDVaSC7A+CLcKnKkYuSjgObaauAFJWdjHmK1X ayushmaan@thegram"
