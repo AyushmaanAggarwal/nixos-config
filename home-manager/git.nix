d1eb9de5d02643b8cbb11e59bc23c809fbf3990c
AyushmaanAggarwal <ayu02468@gmail.com>
2025-06-23 14:31:42 -0700

    Add git file (not being used yet)

---
+++ home-manager/git.nix
@@ -0,0 +1,40 @@
+# Not ready for use
+{
+  inputs,
+  config,
+  pkgs,
+  lib,
+  desktop,
+  hostname,
+  system,
+  username,
+  ...
+}: {
+  programs.git = {
+    enable = true;
+    signing = {
+      key = "";
+      signByDefault = true;
+    };
+    aliases = {
+      new = "checkout -b";
+      s = "status -sb";
+      a = "add .";
+      c = "commit -aS --verbose";
+      ca = "commit -aS --verbose --amend";
+      m = "merge";
+      l = "log";
+    };
+    extraConfig = {
+      init.defaultBranch = "master";
+      push.autoSetupRemote = true;
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+    };
+  };
+
+  programs.ssh = {
+    enable = true;
+    addKeysToAgent = "yes";
+  };
+}
