6daed1323824709ca49a15e46de5d9ce9505ecdb
AyushmaanAggarwal <ayu02468@gmail.com>
2025-06-24 11:50:59 -0700

    Add applications needed for matlab

--- home-manager/shell/distrobox.nix
+++ home-manager/shell/distrobox.nix
@@ -15,6 +15,10 @@
       matlab = {
         image = "ubuntu:24.04";
         entry = true;
+        additional_packages = "xcb alsa-lib libnss3 libgtk2.0-0";
+        exported_apps="Matlab";
+        exported_bins="/home/ayushmaan/Applications/matlab/bin/matlab";
+        exported_bins_path="/home/ayushmaan/Applications/matlab/bin";
       };
     };
   };
