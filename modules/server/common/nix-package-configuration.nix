2a8bc5b7e3eddcd40fb19b68389c7858da3a6bb0
AyushmaanAggarwal <ayu02468@gmail.com>
2025-06-25 13:07:00 -0700

    Add build-host for backup server and fix ssh permissions

--- modules/server/common/nix-package-configuration.nix
+++ modules/server/common/nix-package-configuration.nix
@@ -3,63 +3,73 @@
   outputs,
   config,
   pkgs,
+  lib,
   hostname,
   username,
   desktop,
   system,
   ...
 }: {
-  # Add unstable overlay for newer packages
-  nixpkgs.overlays = [
-    outputs.overlays.unstable-packages
+  config = lib.mkMerge [
+    (lib.mkIf ("${hostname}" != "backup") {
+      system.autoUpgrade.flags = [
+        "--build-host"
+        "proxmox@backup"
+      ];
+    })
+    {
+      # Add unstable overlay for newer packages
+      nixpkgs.overlays = [
+        outputs.overlays.unstable-packages
+      ];
+      
+      # Enable Flakes
+      nix.settings = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        trusted-users = ["nixadmin"];
+        cores = 3;
+        max-jobs = 4;
+      };
+    
+      # Automatic updates
+      system.autoUpgrade = {
+        enable = true;
+        flake = "github:AyushmaanAggarwal/nixos-config#${hostname}";
+        dates = "hourly";
+        randomizedDelaySec = "10min";
+        allowReboot = true;
+        flags = [
+          "--option"
+          "tarball-ttl"
+          "0"
+        ];
+      };
+ 
+      systemd.services.reboot = {
+        enable = true;
+        after = ["nixos-upgrade.service"];
+        description = "Restic Backup System";
+        serviceConfig = {
+          User = "root";
+          ExecStart = ''reboot'';
+        };
+      };
+    
+      # Collect garbage
+      nix.gc = {
+        automatic = true;
+        dates = "01:00";
+        options = "--delete-older-than 30d";
+      };
+    
+      nix.optimise = {
+        automatic = true;
+        dates = ["weekly"];
+      };
+    }
   ];
-  
-  # Enable Flakes
-  nix.settings = {
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    trusted-users = ["nixadmin"];
-    cores = 3;
-    max-jobs = 4;
-  };
 
-  # Automatic updates
-  system.autoUpgrade = {
-    enable = true;
-    flake = "github:AyushmaanAggarwal/nixos-config#${hostname}";
-    dates = "hourly";
-    randomizedDelaySec = "10min";
-    allowReboot = true;
-    flags = [
-      "--build-host"
-      "proxmox@backup"
-      "--option"
-      "tarball-ttl"
-      "0"
-    ];
-  };
-
-  systemd.services.reboot = {
-    enable = true;
-    after = ["nixos-upgrade.service"];
-    description = "Restic Backup System";
-    serviceConfig = {
-      User = "root";
-      ExecStart = ''reboot'';
-    };
-  };
-
-  # Collect garbage
-  nix.gc = {
-    automatic = true;
-    dates = "01:00";
-    options = "--delete-older-than 30d";
-  };
-
-  nix.optimise = {
-    automatic = true;
-    dates = ["weekly"];
-  };
 }
--- modules/server/common/users.nix
+++ modules/server/common/users.nix
@@ -9,80 +9,82 @@
   sshWithoutYubikey,
   ...
 }: let
-  ssh_unsecure = ["thegram"];
   ssh_primary = [
-    "yubikey_type_a" 
-    "yubikey_type_c" 
-    "backup"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIKF5IDpNa1gIA9lNUv/j0p5Yicf68YJUYsxIitpwTfPVAAAABHNzaDo=" # yubikey_type_a
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIOuQHAZ1ATX5y5z3gQHR8Bp52ZBoPmLN1/iap8hYjw7eAAAABHNzaDo=" # yubikey_type_c
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKfo5bbYZcxVwve1XSj+pYaxmdlTKBr8L9dNeCCloZ1M" # backup
   ];
-  ssh_containers = lib.mkIf ("${hostname}" == "backup") [
-    "uptime"
-    "changedetection"
-    "immich"
-    "nextcloud"
-    "adguard"
-    "ntfy"
-    "mealie"
+
+  ssh_unsecure = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEa53AGMV87VUquUKyQ2NlqmZiN7OVV438VLUe6hYJU2" # thegram
+  ];  
+
+  ssh_containers = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJLX97kPiXxKA8W0TFpkkwvtWKY8QSCMyCNSe1PmEl7N" # uptime
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJFgk0VQzuFSzeHfT5Wd4tGQLzRvzCKgeAtvmoU00YAs" # changedetection
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAiDptqCUt/I5DJRSt15RB6p9ZToHgdcT+c3U6aS01r" # immich
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF5tBOnQ7FrDZvlgkKsMIisIEm/eB+Xwc/evqKF5dw3g" # nextcloud
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEuwJYFfBZLG6feRKBFfa7+dWzAUBvmqA++JnJUrj+mG" # adguard
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEo956tAmvf+G4fKmKP6Box+te3vDQe22qMYCLrrWlmE" # ntfy
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICQPeb1/e8Hzr3S5/LwGHNbwjupo5UZ3W3UctpUl1Pyv" # mealie
   ];
 in {
-  imports = [./sops-nix.nix];
-
-  sops.secrets = functions.listToAttrsAttrs (ssh_unsecure ++ ssh_primary ++ ssh_containers) {
-    user = "root";
-    group = "root";
-    mode = "0400";
-    sopsFile = ../../../secrets/general/ssh_keys.yaml;
-  };
-
-  # --------------------
-  # Users
-  # --------------------
-  security.sudo = {
-    enable = true;
-    extraRules = [
-      {
-        commands = [
+  config = lib.mkMerge [
+    {
+      # --------------------
+      # Users
+      # --------------------
+      security.sudo = {
+        enable = true;
+        extraRules = [
           {
-            command = "/run/current-system/sw/bin/nix-env ^-p /nix/var/nix/profiles/system --set /nix/store/[0-9a-z]+-nixos-system-unnamed-(lxc-proxmox-)?[0-9a-g\\.]+$";
-            options = ["NOPASSWD"];
-          }
-          {
-            command = "/run/current-system/sw/bin/systemd-run ^-E LOCALE_ARCHIVE -E NIXOS_INSTALL_BOOTLOADER= --collect --no-ask-password --pipe --quiet --service-type=exec --unit=nixos-rebuild-switch-to-configuration --wait (true|/nix/store/\\S+/bin/switch-to-configuration (switch|boot))$";
-            options = ["NOPASSWD"];
+            commands = [
+              {
+                command = "/run/current-system/sw/bin/nix-env ^-p /nix/var/nix/profiles/system --set /nix/store/[0-9a-z]+-nixos-system-unnamed-(lxc-proxmox-)?[0-9a-g\\.]+$";
+                options = ["NOPASSWD"];
+              }
+              {
+                command = "/run/current-system/sw/bin/systemd-run ^-E LOCALE_ARCHIVE -E NIXOS_INSTALL_BOOTLOADER= --collect --no-ask-password --pipe --quiet --service-type=exec --unit=nixos-rebuild-switch-to-configuration --wait (true|/nix/store/\\S+/bin/switch-to-configuration (switch|boot))$";
+                options = ["NOPASSWD"];
+              }
+            ];
+            users = ["nixadmin"];
           }
         ];
-        users = ["nixadmin"];
-      }
-    ];
-  };
+      };
 
-  # Enable backup server to act as a remote builder
-  programs.ssh.knownHosts.backup.publicKey = config.sops.secrets.backup.path;
+      # Enable backup server to act as a remote builder
+      programs.ssh.knownHosts.backup.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKfo5bbYZcxVwve1XSj+pYaxmdlTKBr8L9dNeCCloZ1M"; # backup
 
-  users.users = {
-    nixadmin = {
-      isNormalUser = true;
-      description = "Nixpkgs User"; # Used as a minimal remote builder
-      extraGroups = ["wheel"];
-      hashedPassword = "$y$j9T$MsKPpS9seZjFQTddCHJ.g0$WeGelFn99zcnxhW.QdoIC.ZslQLxgBm4a7sQKdfBdC7";
-      openssh.authorizedKeys.keyFiles = lib.map (file: config.sops.secrets."${file}".path) ssh_primary;
-    };
+      users.users = {
+        nixadmin = {
+          isNormalUser = true;
+          description = "Nixpkgs User"; # Used as a minimal remote builder
+          extraGroups = ["wheel"];
+          hashedPassword = "$y$j9T$MsKPpS9seZjFQTddCHJ.g0$WeGelFn99zcnxhW.QdoIC.ZslQLxgBm4a7sQKdfBdC7";
+          openssh.authorizedKeys.keys = ssh_primary;
+        };
 
-    proxmox = {
-      isNormalUser = true;
-      description = "Proxmox User"; # Primary non-root user in container
-      extraGroups = [
-        "networkmanager"
-        "wheel"
-      ];
-      packages = with pkgs; [
-        neovim
-        htop
-        powertop
-        fastfetch
-      ];
-      hashedPassword = "$y$j9T$nuV.3iXRhPpKvTXd94fFh.$9g4xyPrktivR.wpwUxT4P69bs0NLLAe2sDWDIjus5c4";
-      openssh.authorizedKeys.keys = lib.map (file: config.sops.secrets."${file}".path) ssh_primary ++ ssh_unsecure ++ ssh_containers;
-    };
-  };
+        proxmox = {
+          isNormalUser = true;
+          description = "Proxmox User"; # Primary non-root user in container
+          extraGroups = [
+            "networkmanager"
+            "wheel"
+          ];
+          packages = with pkgs; [
+            neovim
+            htop
+            powertop
+            fastfetch
+          ];
+          hashedPassword = "$y$j9T$nuV.3iXRhPpKvTXd94fFh.$9g4xyPrktivR.wpwUxT4P69bs0NLLAe2sDWDIjus5c4";
+          openssh.authorizedKeys.keys = ssh_primary ++ ssh_unsecure;
+        };
+      };
+    }
+    (lib.mkIf ("${hostname}" == "backup") {
+      users.users.proxmox.openssh.authorizedKeys.keys = ssh_containers;
+    })
+  ];
 }
