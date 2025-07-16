a5a4462f0ff541cb9bf4fe9fbc6f8ade30105198
AyushmaanAggarwal <ayu02468@gmail.com>
2025-07-15 17:08:43 -0700

    Experiment with extra host tailscale

--- modules/server/services/caddy.nix
+++ modules/server/services/caddy.nix
@@ -3,7 +3,8 @@
   lib,
   pkgs,
   ...
-}: {
+}:
+{
   imports = [
     ./tailscale.nix
   ];
@@ -33,19 +34,39 @@
       description = "Set hostname for tailscale caddy-configuration";
       default = config.networking.hostName;
     };
+    caddy.extraHost = lib.mkOption {
+      type = lib.types.str;
+      description = "Extra Hostname";
+      default = "";
+    };
+    caddy.extra_custom_proxy = lib.mkOption {
+      type = lib.types.str;
+      description = "Set extra custom Caddy reverse proxy";
+      default = "";
+    };
+
   };
 
-  config = lib.mkIf (config.caddy.enable) {
-    # --------------------
-    # Caddy SSL Cert
-    # --------------------
-    services.caddy = {
-      enable = true;
-      virtualHosts."${config.caddy.hostname}.tail590ac.ts.net".extraConfig =
-        if (config.caddy.custom_proxy == "")
-        then ''reverse_proxy ${config.caddy.url}:${toString config.caddy.port}''
-        else config.caddy.custom_proxy;
-    };
-    tailscale.caddycert.enable = true;
-  };
+  config = lib.mkIf (config.caddy.enable) (
+    lib.mkMerge [
+      {
+        # --------------------
+        # Caddy SSL Cert
+        # --------------------
+        services.caddy = {
+          enable = true;
+          virtualHosts."${config.caddy.hostname}.tail590ac.ts.net".extraConfig =
+            if (config.caddy.custom_proxy == "") then
+              ''reverse_proxy ${config.caddy.url}:${toString config.caddy.port}''
+            else
+              config.caddy.custom_proxy;
+        };
+        tailscale.caddycert.enable = true;
+      }
+      (lib.mkIf (config.caddy.extraHost != "") {
+        services.caddy.virtualHosts."${config.caddy.extraHost}".extraConfig =
+          config.caddy.extra_custom_proxy;
+      })
+    ]
+  );
 }
