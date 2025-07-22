be71bd8e05efea777cb11c87b49310af71d2f3c7
AyushmaanAggarwal <ayu02468@gmail.com>
2025-07-22 16:30:02 -0700

    Working on additional no-password commands

--- modules/server/common/users.nix
+++ modules/server/common/users.nix
@@ -8,7 +8,8 @@
   system,
   sshWithoutYubikey,
   ...
-}: let
+}:
+let
   ssh_primary = [
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIKF5IDpNa1gIA9lNUv/j0p5Yicf68YJUYsxIitpwTfPVAAAABHNzaDo=" # yubikey_type_a
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIOuQHAZ1ATX5y5z3gQHR8Bp52ZBoPmLN1/iap8hYjw7eAAAABHNzaDo=" # yubikey_type_c
@@ -17,7 +18,7 @@
 
   ssh_unsecure = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEa53AGMV87VUquUKyQ2NlqmZiN7OVV438VLUe6hYJU2" # thegram
-  ];  
+  ];
 
   ssh_containers = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJLX97kPiXxKA8W0TFpkkwvtWKY8QSCMyCNSe1PmEl7N" # uptime
@@ -28,7 +29,8 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEo956tAmvf+G4fKmKP6Box+te3vDQe22qMYCLrrWlmE" # ntfy
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICQPeb1/e8Hzr3S5/LwGHNbwjupo5UZ3W3UctpUl1Pyv" # mealie
   ];
-in {
+in
+{
   config = lib.mkMerge [
     {
       # --------------------
@@ -41,26 +43,32 @@ in {
             commands = [
               {
                 command = "/run/current-system/sw/bin/nix-env ^-p /nix/var/nix/profiles/system --set /nix/store/[0-9a-z]+-nixos-system-unnamed-(lxc-proxmox-)?[0-9a-g\\.]+$";
-                options = ["NOPASSWD"];
+                options = [ "NOPASSWD" ];
               }
               {
                 command = "/run/current-system/sw/bin/systemd-run ^-E LOCALE_ARCHIVE -E NIXOS_INSTALL_BOOTLOADER= --collect --no-ask-password --pipe --quiet --service-type=exec --unit=nixos-rebuild-switch-to-configuration --wait (true|/nix/store/\\S+/bin/switch-to-configuration (switch|boot))$";
-                options = ["NOPASSWD"];
+                options = [ "NOPASSWD" ];
               }
+              {
+                command = "/run/current-system/sw/bin/env ^NIXOS_INSTALL_BOOTLOADER=0 /run/current-system/sw/bin/systemd-run -E LOCALE_ARCHIVE -E NIXOS_INSTALL_BOOTLOADER= --collect --no-ask-password --pipe --quiet --service-type=exec --unit=nixos-rebuild-switch-to-configuration --wait (true|/nix/store/\\S+/bin/switch-to-configuration (switch|boot))$";
+                options = [ "NOPASSWD" ];
+              }
+
             ];
-            users = ["nixadmin"];
+            users = [ "nixadmin" ];
           }
         ];
       };
 
       # Enable backup server to act as a remote builder
-      programs.ssh.knownHosts.backup.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKfo5bbYZcxVwve1XSj+pYaxmdlTKBr8L9dNeCCloZ1M"; # backup
+      programs.ssh.knownHosts.backup.publicKey =
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKfo5bbYZcxVwve1XSj+pYaxmdlTKBr8L9dNeCCloZ1M"; # backup
 
       users.users = {
         nixadmin = {
           isNormalUser = true;
           description = "Nixpkgs User"; # Used as a minimal remote builder
-          extraGroups = ["wheel"];
+          extraGroups = [ "wheel" ];
           hashedPassword = "$y$j9T$MsKPpS9seZjFQTddCHJ.g0$WeGelFn99zcnxhW.QdoIC.ZslQLxgBm4a7sQKdfBdC7";
           openssh.authorizedKeys.keys = ssh_primary;
         };
