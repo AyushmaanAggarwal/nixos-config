f9fc6535424a480f75d79e3e1e174bb1a5b2cca1
AyushmaanAggarwal <ayu02468@gmail.com>
2025-07-27 21:11:50 -0700

    Add several settings to gtk theming

--- home-manager/theme.nix
+++ home-manager/theme.nix
@@ -1,48 +1,49 @@
-{
-  pkgs,
-  home-manager,
-  ...
-}:
-{
+{ pkgs, home-manager, ... }: {
   home.sessionVariables = {
     EDITOR = "nvim";
     GTK_THEME = "Adwaita:dark";
   };
 
   dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
+    "org/gnome/desktop/interface" = { color-scheme = "prefer-dark"; };
   };
 
-  gtk.gtk3.extraConfig = {
-    gtk-application-prefer-dark-theme = true;
-    gtk-button-images = true;
-    gtk-cursor-theme-name = "breeze_cursors";
-    gtk-cursor-theme-size = 24;
-    gtk-decoration-layout = "icon:minimize,maximize,close";
-    gtk-enable-animations = true;
-    gtk-font-name = "Noto Sans,  10";
-    gtk-icon-theme-name = "breeze";
-    gtk-menu-images = true;
-    gtk-modules = "colorreload-gtk-module";
-    gtk-primary-button-warps-slider = true;
-    gtk-sound-theme-name = "ocean";
-    gtk-toolbar-style = 3;
-    gtk-xft-dpi = 122880;
-  };
-  gtk.gtk4.extraConfig = {
-    gtk-application-prefer-dark-theme = true;
-    gtk-cursor-theme-name = "breeze_cursors";
-    gtk-cursor-theme-size = 24;
-    gtk-decoration-layout = "icon:minimize,maximize,close";
-    gtk-enable-animations = true;
-    gtk-font-name = "Noto Sans,  10";
-    gtk-icon-theme-name = "breeze";
-    gtk-modules = "colorreload-gtk-module";
-    gtk-primary-button-warps-slider = true;
-    gtk-sound-theme-name = "ocean";
-    gtk-xft-dpi = 122880;
+  gtk = {
+    enable = true;
+    cursorTheme = {
+      name = "Adwaita";
+      size = 24;
+      package = pkgs.adwaita-icon-theme;
+    };
+    font = {
+      name = "Noto Sans";
+      size = 10;
+      package = pkgs.noto-fonts;
+    };
+    iconTheme = {
+      name = "Adwaita-Dark";
+      package = pkgs.adwaita-icon-theme;
+    };
+    theme = {
+      name = "Adwaita-Dark";
+      package = pkgs.gnome.gnome-themes-extra;
+    };
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+      gtk-button-images = true;
+      gtk-enable-animations = true;
+      gtk-menu-images = true;
+      gtk-primary-button-warps-slider = true;
+      gtk-toolbar-style = 3;
+      gtk-xft-dpi = 122880;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+      gtk-enable-animations = true;
+      gtk-primary-button-warps-slider = true;
+      gtk-xft-dpi = 122880;
+    };
   };
 
   qt = {
