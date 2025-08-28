23fa66e80522926b89fe7994a6382419045ed02b
AyushmaanAggarwal <ayu02468@gmail.com>
2025-08-28 14:31:20 -0700

    test: Change from latest to zen kernel
    This change will supposedly bring better responsiveness in the kernel

--- modules/system/core/boot.nix
+++ modules/system/core/boot.nix
@@ -10,7 +10,8 @@
   boot.kernelParams = [
     "quiet"
   ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot = {
     enable = true;
