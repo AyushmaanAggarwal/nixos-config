36f1dbde7cfd63c1990adeccd1649676d75097f1
AyushmaanAggarwal <ayu02468@gmail.com>
2025-10-22 14:48:09 -0700

    feat: Add zfs kernel

--- modules/system/core/boot.nix
+++ modules/system/core/boot.nix
@@ -5,13 +5,27 @@
   pkgs,
   ...
 }:
+let
+  zfsCompatibleKernelPackages = lib.filterAttrs (
+    name: kernelPackages:
+    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+    && (builtins.tryEval kernelPackages).success
+    && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+  ) pkgs.linuxKernel.packages;
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues zfsCompatibleKernelPackages
+    )
+  );
+in
 {
   # Bootloader.
   boot.kernelParams = [
     "quiet"
   ];
   #boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  #boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = latestKernelPackage;
   boot.loader = {
     efi.canTouchEfiVariables = true;
     timeout = 5;
