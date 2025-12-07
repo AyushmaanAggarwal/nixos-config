fa08b321bc40fd8e47ef2fc53e1a3da569b4d97e
AyushmaanAggarwal <ayu02468@gmail.com>
2025-12-06 19:46:24 -0800

    fix: bug in building overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,9 +6,15 @@
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
   unstable-packages = final: _prev: {
-    unstable = import inputs.nixpkgs-unstable;
+    unstable = import inputs.nixpkgs-unstable {
+      system = final.stdenv.hostPlatform.system;
+      config.allowUnfree = true;
+    };
   };
   stable-packages = final: _prev: {
-    stable = import inputs.nixpkgs-stable;
+    stable = import inputs.nixpkgs-stable {
+      system = final.stdenv.hostPlatform.system;
+      config.allowUnfree = true;
+    };
   };
 }
