730d26ae1ec5a003278449377fdd34daef661888
AyushmaanAggarwal <ayu02468@gmail.com>
2025-12-19 09:44:45 -0800

    searxng: change request timeout

--- modules/server/services/searxng.nix
+++ modules/server/services/searxng.nix
@@ -117,75 +117,79 @@ in
 
         # Search engines
         # engines = lib.mapAttrsToList (name: value: { inherit name; } // value) {
+        #   # enabled
         #   "duckduckgo".disabled = false;
-        #   "brave".disabled = true;
-        #   "bing".disabled = false;
-        #   "mojeek".disabled = true;
+
         #   "mwmbl".disabled = false;
         #   "mwmbl".weight = 0.4;
-        #   "qwant".disabled = true;
+        #   "bing".disabled = false;
         #   "crowdview".disabled = false;
         #   "crowdview".weight = 0.5;
-        #   "curlie".disabled = true;
         #   "ddg definitions".disabled = false;
         #   "ddg definitions".weight = 2;
         #   "wikibooks".disabled = false;
         #   "wikidata".disabled = false;
-        #   "wikiquote".disabled = true;
-        #   "wikisource".disabled = true;
         #   "wikispecies".disabled = false;
         #   "wikispecies".weight = 0.5;
         #   "wikiversity".disabled = false;
         #   "wikiversity".weight = 0.5;
         #   "wikivoyage".disabled = false;
         #   "wikivoyage".weight = 0.5;
-        #   "currency".disabled = true;
-        #   "dictzone".disabled = true;
-        #   "lingva".disabled = true;
         #   "bing images".disabled = false;
-        #   "brave.images".disabled = true;
-        #   "duckduckgo images".disabled = true;
+        #
         #   "google images".disabled = false;
-        #   "qwant images".disabled = true;
-        #   "1x".disabled = true;
         #   "artic".disabled = false;
         #   "deviantart".disabled = false;
-        #   "flickr".disabled = true;
         #   "imgur".disabled = false;
         #   "library of congress".disabled = false;
-        #   "material icons".disabled = true;
-        #   "material icons".weight = 0.2;
         #   "openverse".disabled = false;
-        #   "pinterest".disabled = true;
         #   "svgrepo".disabled = false;
         #   "unsplash".disabled = false;
         #   "wallhaven".disabled = false;
         #   "wikicommons.images".disabled = false;
-        #   "yacy images".disabled = true;
         #   "bing videos".disabled = false;
-        #   "brave.videos".disabled = true;
-        #   "duckduckgo videos".disabled = true;
         #   "google videos".disabled = false;
         #   "qwant videos".disabled = false;
+        #   "peertube".disabled = false;
+        #   "rumble".disabled = false;
+        #   "sepiasearch".disabled = false;
+        #   "youtube".disabled = false;
+        #   # disabled
+        #   "brave".disabled = true;
+        #   "mojeek".disabled = true;
+        #   "qwant".disabled = true;
+        #   "curlie".disabled = true;
+        #   "wikiquote".disabled = true;
+        #   "wikisource".disabled = true;
+        #   "currency".disabled = true;
+        #   "dictzone".disabled = true;
+        #   "lingva".disabled = true;
+        #   "brave.images".disabled = true;
+        #   "duckduckgo images".disabled = true;
+        #   "qwant images".disabled = true;
+        #   "1x".disabled = true;
+        #   "flickr".disabled = true;
+        #   "material icons".disabled = true;
+        #   "material icons".weight = 0.2;
+        #   "pinterest".disabled = true;
+        #   "yacy images".disabled = true;
+        #   "brave.videos".disabled = true;
+        #   "duckduckgo videos".disabled = true;
         #   "dailymotion".disabled = true;
         #   "google play movies".disabled = true;
         #   "invidious".disabled = true;
         #   "odysee".disabled = true;
-        #   "peertube".disabled = false;
         #   "piped".disabled = true;
-        #   "rumble".disabled = false;
-        #   "sepiasearch".disabled = false;
         #   "vimeo".disabled = true;
-        #   "youtube".disabled = false;
         #   "brave.news".disabled = true;
         #   "google news".disabled = true;
         # };
 
         # Outgoing requests
         outgoing = {
-          request_timeout = 5.0;
+          request_timeout = 1.0;
           max_request_timeout = 15.0;
-          pool_connections = 100;
+          pool_connections = 200;
           pool_maxsize = 15;
           enable_http2 = true;
         };
