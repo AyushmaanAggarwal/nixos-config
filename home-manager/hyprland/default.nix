32b42da05d3cd90d559ac35381b1af7513653667
AyushmaanAggarwal <ayu02468@gmail.com>
2026-03-25 00:47:30 -0700

    Add noctalia

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -10,8 +10,10 @@ in
     ./fuzzel.nix
     ./waybar/default.nix
     ./profile.nix
+    ./noctalia.nix
   ];
   fuzzel.enable = enable-hyprland;
   waybar.enable = enable-hyprland;
   hyprland-profile.enable = enable-hyprland;
+  noctalia.enable = enable-hyprland;
 }
