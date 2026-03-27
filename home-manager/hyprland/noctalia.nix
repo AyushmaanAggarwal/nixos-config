6f9b802e46b28fde2ca0e3c43612122bbc8abb21
AyushmaanAggarwal <ayu02468@gmail.com>
2026-03-26 21:40:27 -0700

    Add noctalia bar

--- home-manager/hyprland/noctalia.nix
+++ home-manager/hyprland/noctalia.nix
@@ -21,7 +21,58 @@
     };
   };
   config = lib.mkIf (config.noctalia.enable) {
-    programs.noctalia-shell.enable = true;
-
+    programs.noctalia-shell = {
+      enable = true;
+      settings = {
+        # configure noctalia here
+        bar = {
+          density = "compact";
+          position = "right";
+          showCapsule = false;
+          widgets = {
+            left = [
+              {
+                id = "ControlCenter";
+                useDistroLogo = true;
+              }
+              {
+                id = "Network";
+              }
+              {
+                id = "Bluetooth";
+              }
+            ];
+            center = [
+              {
+                hideUnoccupied = false;
+                id = "Workspace";
+                labelMode = "none";
+              }
+            ];
+            right = [
+              {
+                alwaysShowPercentage = false;
+                id = "Battery";
+                warningThreshold = 30;
+              }
+              {
+                formatHorizontal = "HH:mm";
+                formatVertical = "HH mm";
+                id = "Clock";
+                useMonospacedFont = true;
+                usePrimaryColor = true;
+              }
+            ];
+          };
+        };
+        colorSchemes.predefinedScheme = "Monochrome";
+        general = {
+        };
+        location = {
+          monthBeforeDay = true;
+          name = "San Jose, USA";
+        };
+      };
+    };
   };
 }
--- modules/system/core/package-manager.nix
+++ modules/system/core/package-manager.nix
@@ -17,10 +17,12 @@
     substituters = [
       "https://hyprland.cachix.org"
       "https://cache.nixos.org/"
+      "https://noctalia.cachix.org"
     ];
 
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
 
     max-jobs = 4;
